6c8535d15f5ae6c9b6aa8ec723d0dd8d41f53091
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2023-11-21 10:40:29 +0000

    Made some updates to the checking account class

--- src/bankingapp/Checking.java
+++ src/bankingapp/Checking.java
@@ -2,8 +2,8 @@ package bankingapp;
 
 public class Checking extends Account {
 
-    private int debitCardNumber;
-    private int debitCardPin;
+    int debitCardNumber;
+    int debitCardPin;
 
     public Checking(String name, String sSN, double initDeposit) {
         super(name, sSN, initDeposit);
@@ -13,11 +13,13 @@ public class Checking extends Account {
 
     private void setDebitCard() {
         debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
-        debitCardPin = (int) (Math.random() * Math.pow(10, 12));
+        debitCardPin = (int) (Math.random() * Math.pow(10, 4));
     }
 
     public void showInfo(){
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Checking");
+        System.out.println("Checking Account Info "
+          + "\n Debit Card Number: " + debitCardNumber +
+           "\n Debit Card PIN " + debitCardPin);
     }
 }
